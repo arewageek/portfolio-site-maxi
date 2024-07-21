ca4ef5740702df376157c932737a2d0d6702e942
Austin Ameh <arewageek@gmail.com>
2024-07-21 01:03:38 +0100

    disabled work page and slides

--- src/components/layouts/MobileNav.tsx
+++ src/components/layouts/MobileNav.tsx
@@ -9,7 +9,7 @@ const links = [
     { name: 'home', path: '/' },
     { name: 'services', path: '/services' },
     { name: 'resume', path: '/resume' },
-    { name: 'work', path: '/work' },
+    // { name: 'work', path: '/work' },
     // { name: 'contact', path: '/contact' },
 ]
 
--- src/components/layouts/Nav.tsx
+++ src/components/layouts/Nav.tsx
@@ -7,7 +7,7 @@ export const links = [
     { name: 'home', path: '/' },
     { name: 'services', path: '/services' },
     { name: 'resume', path: '/resume' },
-    { name: 'work', path: '/work' },
+    // { name: 'work', path: '/work' },
     // { name: 'contact', path: '/contact' },
 ]
 
